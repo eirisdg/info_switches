7c537eb46d62e2e10cd6276d58d772054802f9be
Mario Adrián Domínguez González <tupacsoul@gmail.com>
2017-03-07 09:49:10 +0100

    Añadido get_ports del 1510-28

--- Utils/Switches/d151028.py
+++ Utils/Switches/d151028.py
@@ -9,7 +9,7 @@ class D151028(Switch):
     def __init__(self, f0, ipsw):
         super(D151028,self).__init__(f0, ipsw)
 
-    # (['X/xx', 'UP/DOWN'],['X/xx', 'UP/DOWN'],['X/xx', 'UP/DOWN'],['X/xx', 'UP/DOWN'])
+    # ([['DGS-1510-28']['X/xx', 'UP/DOWN'],['X/xx', 'UP/DOWN']],[['DGS-1510-28']['X/xx', 'UP/DOWN'],['X/xx', 'UP/DOWN']])
     def get_ports_status(self,ssh):
         sshtransport = ssh.get_transport()
         local_addr = (self.f0, 22)
@@ -20,14 +20,35 @@ class D151028(Switch):
         sw.set_missing_host_key_policy(AutoAddPolicy())
         sw.connect(self.f0, username='admin', password='ceycswtic', sock=sshchannel, timeout=5)
 
-        interact = SSHClientInteraction(sw, timeout=1, display=True)
-        interact.expect(['Switches#', 'Switch0#'])
+        interact = SSHClientInteraction(sw, timeout=1, display=False)
+        interact.expect(['Switch#', 'Switch0#'])
         interact.send('show interface status')
         interact.send('a')
-        interact.expect(['Switches#', 'Switch0#'])
+        interact.expect(['Switch#', 'Switch0#'])
         interact.send('logout')
-        puertos = interact.current_output_clean
+        interact.close()
+        sw.close()
+        salida = interact.current_output_clean
 
+        stack = []
+        switch = []
+        switch.append('DGS-1510-28')
+        for line in salida.splitlines():
+            if 'eth' in line:
+                unit = line.split('eth')[1][0]
+                boca = line.split('eth')[1][4:6]
+                if 'not-connected' in line:
+                    status = 'Down'
+                else:
+                    status = 'Up'
 
-
-        return(puertos)
+                if len(switch) > 1 and unit != switch[-1][0]:
+                    stack.append(switch)
+                    switch = []
+                    switch.append('DGS-1510')
+                    switch.append([unit, boca, status])
+                else:
+                    switch.append([unit, boca, status])
+            elif 'Total Entries' in line:
+                stack.append(switch)
+        return(stack)
